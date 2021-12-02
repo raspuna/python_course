3eb415557c5af4fd7dbddcd635ec119f22f8c931
raspuna <raspuna@gmail.com>
2021-12-02 14:25:04 -0800

    function intermediate assignment

---
+++ python/fundamentals/fundamentals/function_intermediate/function_intermediate.py
@@ -0,0 +1,66 @@
+# 1
+
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0] = 15
+students[0]['last_name'] = 'Bryant'
+sports_directory['soccer'][0] = 'Andres'
+z[0]['y'] = 30
+
+print(x)
+print(students)
+print(sports_directory)
+print(z)
+
+# 2
+
+def iterateDictionary(some_list):
+    for a_dict in some_list:
+        print(f"first_name - {a_dict['first_name']}, last_name - {a_dict['last_name']}")
+
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+iterateDictionary(students) 
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+"""
+first_name - Michael, last_name - Jordan
+first_name - John, last_name - Rosales
+first_name - Mark, last_name - Guillen
+first_name - KB, last_name - Tonel
+"""
+
+# 3
+def iterateDictionary2(key_name, some_list):
+    for a_dict in some_list:
+        print(a_dict[key_name])
+
+iterateDictionary2('first_name', students)
+iterateDictionary2('last_name', students)
+
+#4 
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+def printInfo(some_dict):
+    for key, value in some_dict.items():
+        print("-" * 14)
+        print(len(value), key.upper())
+        for v in value:
+            print(v)
+
+printInfo(dojo)
