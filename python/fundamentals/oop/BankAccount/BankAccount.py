6728935946e4593e94e2bd93813475bd3ba3e7a6
raspuna <raspuna@gmail.com>
2021-12-03 15:31:21 -0800

    added missing yield, rounded result

--- python/fundamentals/oop/BankAccount/BankAccount.py
+++ python/fundamentals/oop/BankAccount/BankAccount.py
@@ -21,7 +21,7 @@ class BankAccount:
         return self
 
     def display_account_info(self):
-        print(f"Balance: ${self.balance}")
+        print(f"Balance: ${round(float(self.balance), 2)}")
     
     def yield_interest(self):
         if self.balance > 0:
@@ -31,14 +31,14 @@ class BankAccount:
     @classmethod
     def all_balances(cls):
         for account in cls.all_accounts:
-            print(f"Balance: ${account.balance}")
+            print(f"Balance: ${round(float(account.balance), 2)}")
 
 
 ann = BankAccount(0.01, 250)
 ben = BankAccount(0.02, 3000)
 
 ann.deposit(10).deposit(20).deposit(30).withdraw(35).yield_interest().display_account_info()
-ben.deposit(2500).deposit(10).withdraw(1100).withdraw(175).withdraw(643.5).withdraw(444).display_account_info()
+ben.deposit(2500).deposit(10).withdraw(1100).withdraw(175).withdraw(643.5).withdraw(444).yield_interest().display_account_info()
 
 
 BankAccount.all_balances()
