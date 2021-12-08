7c7d2ac445d1279ef28ac0c4fe6403fb4a7f41d0
raspuna <raspuna@gmail.com>
2021-12-08 13:58:34 -0800

    removed redundant space

--- python/fundamentals/fundamentals/function_intermediate/function_intermediate.py
+++ python/fundamentals/fundamentals/function_intermediate/function_intermediate.py
@@ -2,8 +2,8 @@
 
 x = [ [5,2,3], [10,8,9] ] 
 students = [
-     {'first_name':  'Michael', 'last_name' : 'Jordan'},
-     {'first_name' : 'John', 'last_name' : 'Rosales'}
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
 ]
 sports_directory = {
     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
