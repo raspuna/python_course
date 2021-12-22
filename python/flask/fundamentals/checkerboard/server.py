291d0a6019dbfa3cf7668341caa65eac46693e05
raspuna <raspuna@gmail.com>
2021-12-21 16:17:31 -0800

    checkerboard assignment add

--- python/flask/fundamentals/checkerboard/server.py
+++ python/flask/fundamentals/checkerboard/server.py
@@ -5,11 +5,14 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/<int:y>")
 @app.route("/<int:x>/<int:y>")
-@app.route("/<int:x>/<int:y>/<string:color1>")
-@app.route("/<int:x>/<int:y>/<string:color1>/<string:color2>")
-def checkerboard(x=8,y=8, color1='black', color2='red'):
-    print(x,y,color1,color2)
-    return render_template("checkerboard.html", x=x, y=y, color1=color1, color2=color2)
+def checkerboard(x=8,y=8):
+    return render_template("checkerboard.html", x=x, y=y)
+
+
+@app.route("/<int:x>/<int:y>/color/<string:color1>")
+@app.route("/<int:x>/<int:y>/color/<string:color1>/<string:color2>")
+def checkerboard2(x=8,y=8, color1='black', color2='red'):
+    return render_template("checkerboard2.html", x=x, y=y, color1=color1, color2=color2)
 
 if __name__ == "__main__":
     app.run(debug=True)
